a5a4cb59a151511d0d1d5e3b6a390b59fef07181
Sabrina <113538071+sbrn-9@users.noreply.github.com>
2023-07-11 21:21:21 -0300

    Add files via upload

---
+++ TPGrupo 6_Integrador de ProgramaciónIII/App.Core.Data/MailRepository.cs
@@ -0,0 +1,43 @@
+﻿using App.Core.Entities;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Core.Data
+{
+    public class MailRepository
+    {
+        public MailRepository()
+        {
+                
+        }
+
+        
+        public List<Mail> Search(string textToSearch, int pageSize, int pageIndex, string destinatario, string remitente)
+        {
+            var skipRows = ((pageIndex - 1) * pageSize);
+
+            using (var context = new MailsContext())
+            {
+                
+                var query = from m in context.Mails
+                            where (m.Asunto.Contains(textToSearch) ||
+                                   m.Destinatario.Contains(textToSearch) ||
+                                   m.Remitente.Contains(textToSearch) ||
+                                   m.Contenido.Contains(textToSearch)) && ((m.Destinatario == destinatario && m.Remitente == remitente) ||
+                                  (m.Destinatario == remitente && m.Remitente == destinatario))
+                            select m;
+
+                return query.Skip(skipRows)
+                            .Take(pageSize)
+                            .ToList();
+            }
+        }
+
+        
+
+    }
+}
