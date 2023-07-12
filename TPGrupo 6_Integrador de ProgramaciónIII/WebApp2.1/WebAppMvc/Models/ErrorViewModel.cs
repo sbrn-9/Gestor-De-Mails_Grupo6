da4a0c60d8d8f8759aab5ab471388ee3619cbd97
Sabrina <113538071+sbrn-9@users.noreply.github.com>
2023-07-11 21:40:29 -0300

    Delete TPGrupo 6_Integrador de ProgramaciónIII/WebApp2.1 directory

--- TPGrupo 6_Integrador de ProgramaciónIII/WebApp2.1/WebAppMvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAppMvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
