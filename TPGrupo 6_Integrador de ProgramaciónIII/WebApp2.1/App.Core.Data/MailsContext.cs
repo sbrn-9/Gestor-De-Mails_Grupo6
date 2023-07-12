f7b99d6578fb2ebae282fd39b7ba81f8f7c8d802
Sabrina <113538071+sbrn-9@users.noreply.github.com>
2023-07-11 21:24:46 -0300

    Add files via upload

---
+++ TPGrupo 6_Integrador de ProgramaciónIII/WebApp2.1/App.Core.Data/MailsContext.cs
@@ -0,0 +1,24 @@
+﻿
+using App.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Core.Data
+{
+    public class MailsContext: DbContext
+    {
+        public DbSet<Mail> Mails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString =
+                "Persist Security Info=True;Initial Catalog=Mails;Data Source=.; Integrated Security=True;TrustServerCertificate=True;";
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ TPGrupo 6_Integrador de ProgramaciónIII/WebApp2.1/App.Core.Data/UsuarioActual.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Core.Data
+{
+    public class UsuarioActual
+    {
+        public static string NombreUsuario { get; set; }
+    }
+}
